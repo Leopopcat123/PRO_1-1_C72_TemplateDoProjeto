e02ae7517f05b976f1d57fbf5dfbda9e2d416f0c
Leopopcat123 <113531806+Leopopcat123@users.noreply.github.com>
2024-05-08 22:23:00 -0300

    Update Ride.js

--- screens/Ride.js
+++ screens/Ride.js
@@ -229,7 +229,7 @@ export default class RideScreen extends Component {
             <TextInput
               style={[styles.textinput, { width: "82%" }]}
 
-              //onChangeText={() => this.setState({ userId: text })}
+              onChangeText={() => this.setState({ userId: text })}
               //onChangeText={this.setState({ userId: text })}
               //onChangeText={text => this.setState({ userId: text })}
               //onChangeText={(text) => this.setState({ userId: text })}
